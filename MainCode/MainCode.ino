110582757643c1ed50a0909b0ec8adfaa38eeee7
BACH1952 <trannguyenbach195@gmail.com>
2020-06-27 10:39:28 +0700

    bach ghghgh

--- MainCode/MainCode.ino
+++ MainCode/MainCode.ino
@@ -58,6 +58,30 @@ void loop() {
   /*\Code dieu khien dieu hoa*/
   
   /*Code cam bien am thanh*/
-   
+  	int soundSensor=2;
+int LED=4;
+boolean LEDStatus=false;
+
+void setup() {
+ pinMode(soundSensor,INPUT);
+ pinMode(LED,OUTPUT);
+
+}
+
+void loop() {
+
+  int SensorData=digitalRead(soundSensor); 
+  if(SensorData==1){
+
+    if(LEDStatus==false){
+        LEDStatus=true;
+        digitalWrite(LED,HIGH);
+    }
+    else{
+        LEDStatus=false;
+        digitalWrite(LED,LOW);
+    }
+  }
+ } 
   /*\Code cam bien am thanh*/
 }
